896bea7c1a8ab41283126e043da4c9cd25d2b5c4
vonqo <lupino.liberty@gmail.com>
2020-01-29 12:25:56 +0800

    install script

--- install.sh
+++ install.sh
@@ -1,7 +1,76 @@
 #!/bin/bash
+echo ""
+echo "====================================================="
+echo ""
+echo "    _____ ______  _____ _______       _   _______"
+echo "   / ____|  ____|/ ____|__   __|/\   | | |__   __|"
+echo "  | |  __| |__  | (___    | |  /  \  | |    | |"
+echo "  | | |_ |  __|  \___ \   | | / /\ \ | |    | |"
+echo "  | |__| | |____ ____) |  | |/ ____ \| |____| |"
+echo "   \_____|______|_____/   |_/_/    \_\______|_|"
+echo ""
+echo "====================================================="
+echo "  An open source repository of creative coding playground, including interactive design and generative art experiments"
+echo "  https://github.com/vonqo/gestalt"
+echo "  Enkh-Amar.G - Mozilla Public License Version 2.0"
+echo ""
+echo "  [WARNING]: If things go wrong. Screw this script and go manual!"
+echo "  [detecting OS]"
 
-#TODO: detect OS
-#TODO: build moodbar and hqz
-#TODO: build orchestator
-#TODO: link path
+##############################
+## Linux
+##############################
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "  OS: Linux-GNU"
+    ##############################
+    ## Linux Install
+    ##############################
+    
+    ##############################
+    ## Linux Build
+    ##############################
+    echo "  [building moodbar]"
+    cd modules/moodbar/
+    meson --buildtype=release build/
+    cd build/
+    ninja
+    sudo ninja install
+    cd ../../
+    echo "  [building zenphoton]"
+    cd modules/zenphoton/hqz
+    cd ../../../
+    make
+    echo "  [building orchestrator]"
+    cd modules/orchestrator
+    mvn package
 
+##############################
+# Darwin
+##############################
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "  OS: Darwin / Mac OSX"
+    ##############################
+    ## Darwin Install
+    ##############################
+    
+    ##############################
+    ## Darwin Build
+    ##############################
+    echo "  [building moodbar]"
+    cd modules/moodbar-xcode/
+    xcodebuild
+    cd ../../
+    echo "  [building zenphoton]"
+    cd modules/zenphoton/hqz
+    cd ../../../
+    make
+    echo "  [building orchestrator]"
+    cd modules/orchestrator
+    mvn package
+    else
+
+##############################
+## Eto GG
+##############################
+echo "  [ERROR]: Your operating system is not supported!"
+fi
