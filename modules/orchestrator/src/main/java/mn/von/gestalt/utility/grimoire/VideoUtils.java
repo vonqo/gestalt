5d32f6ae26001917368101d6bd867451240261b5
vonqo <lupino.liberty@gmail.com>
2020-03-06 23:58:13 +0800

    ffmpeg control

--- modules/orchestrator/src/main/java/mn/von/gestalt/utility/grimoire/VideoUtils.java
+++ modules/orchestrator/src/main/java/mn/von/gestalt/utility/grimoire/VideoUtils.java
@@ -3,26 +3,64 @@ package mn.von.gestalt.utility.grimoire;
 import mn.von.gestalt.utility.config.Config;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class VideoUtils {
 
-    public static void encodeToVideo(String frameDir, String outputVideo) throws IOException {
+    private int fps;
+    private int imageWidth;
+    private int imageHeight;
+    private String framePrefix;
+    private File audio;
+
+    public VideoUtils() {
+        this.fps = 25;
+        this.imageHeight = 1080;
+        this.imageWidth = 1080;
+        this.framePrefix = "test_";
+    }
+
+    public VideoUtils(int fps, int imageWidth, int imageHeight, String framePrefix, File audio) {
+        this.fps = fps;
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
+        this.framePrefix = framePrefix;
+        this.audio = audio;
+    }
+
+    public void encodeToVideo(String frameDir, String outputVideo) throws IOException {
+        List<String> ffmpegParams = new ArrayList<String>();
+        String[] videoParams = {
+                Config.FFMEG_EXEC,
+                "-r", String.valueOf(this.fps),
+                "-f", "image2",
+                "-s", imageWidth+"x"+imageHeight,
+                "-i", this.framePrefix+"%04d.png",
+                "-vcodec", "libx264",
+                "-crf", "18",
+                "-pix_fmt", "yuv420p",
+                outputVideo
+        };
+        ffmpegParams.addAll(Arrays.asList(videoParams));
+
+        if(audio != null) {
+            String[] audioParams = {
+                "-i", this.audio.getName(),
+                "-acodec", "copy"
+            };
+            ffmpegParams.addAll(Arrays.asList(audioParams));
+        }
 
         ProcessBuilder processBuilder;
         Process process;
-        processBuilder = new ProcessBuilder(
-                Config.FFMEG_EXEC,
-                "-r", "1/5",
-                "-i", "img%03d.png",
-                "-c:v", "libx264",
-                "-vf", "fps=25",
-                "-pix_fmt", "yuv420p",
-                "out.mp4"
-        ).redirectErrorStream(true);
+        processBuilder = new ProcessBuilder(ffmpegParams).redirectErrorStream(true);
         process = processBuilder.start();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
